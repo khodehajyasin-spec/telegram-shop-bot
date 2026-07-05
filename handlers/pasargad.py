ea19f1967387bcb65ac4060962d985439af51892
khodehajyasin-spec <khodehajyasin@gmail.com>
2026-07-05 15:27:14 +0330

    Create handlers/pasargad.py

---
+++ handlers/pasargad.py
@@ -0,0 +1,18 @@
+import requests
+
+PASARGAD_URL = ""
+PASARGAD_USERNAME = ""
+PASARGAD_PASSWORD = ""
+
+class PasargadAPI:
+    def __init__(self):
+        self.token = None
+
+    def login(self):
+        pass
+
+    def create_user(self):
+        pass
+
+    def get_subscription(self):
+        pass
