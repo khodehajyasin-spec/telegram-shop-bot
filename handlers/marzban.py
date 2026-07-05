2413c5d057004a21dfa257081abead6770704ab3
khodehajyasin-spec <khodehajyasin@gmail.com>
2026-07-05 15:25:57 +0330

    Create handlers/marzban.py

---
+++ handlers/marzban.py
@@ -0,0 +1,18 @@
+import requests
+
+MARZBAN_URL = ""
+MARZBAN_USERNAME = ""
+MARZBAN_PASSWORD = ""
+
+class MarzbanAPI:
+    def __init__(self):
+        self.token = None
+
+    def login(self):
+        pass
+
+    def create_user(self):
+        pass
+
+    def get_subscription(self):
+        pass
