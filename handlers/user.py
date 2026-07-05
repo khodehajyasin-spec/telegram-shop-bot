7e176f3b8e4127068290a3ffe56574245891ea7f
khodehajyasin-spec <khodehajyasin@gmail.com>
2026-07-05 15:41:55 +0330

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -1,5 +1,8 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-async def start_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("در حال بارگذاری...")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👋 خوش آمدید.\n\n"
+        "به زودی ربات فروش VPN آماده خواهد شد."
+    )
