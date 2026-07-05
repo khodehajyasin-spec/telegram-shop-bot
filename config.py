0f58bd05a173f280c6d9ea5b15229a27fcd2e2c2
khodehajyasin-spec <khodehajyasin@gmail.com>
2026-07-05 15:39:41 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,34 @@
-CARD_NUMBER = "6219861831024566"
-CARD_OWNER = "احمد محمودی"
-ADMIN_USERNAME = "@config_admin_starlink"
-SHOP_NAME = "V2ray_shop"
+# Telegram
+TOKEN = ""
+
+# Shop
+SHOP_NAME = "V2ray Shop Pro"
+
+# Admin
+ADMIN_ID = 0
+ADMIN_USERNAME = ""
+
+# Card
+CARD_NUMBER = ""
+CARD_OWNER = ""
+
+# Free Test
+FREE_TEST_VOLUME = 25
+FREE_TEST_DURATION = 1
+
+# Marzban
+MARZBAN_URL = ""
+MARZBAN_USERNAME = ""
+MARZBAN_PASSWORD = ""
+
+# Pasargad
+PASARGAD_URL = ""
+PASARGAD_USERNAME = ""
+PASARGAD_PASSWORD = ""
+
+# Database
+DATABASE_NAME = "users.db"
+
+# Languages
+DEFAULT_LANGUAGE = "fa"
+
